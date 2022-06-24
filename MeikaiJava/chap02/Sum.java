e94b098c11d872e72a129d590900ab63af07dc06
aki10-10 <97033820+aki10-10@users.noreply.github.com>
2022-06-24 15:45:26 +0900

    Sum.java

---
+++ MeikaiJava/chap02/Sum.java
@@ -0,0 +1,10 @@
+package MeikaiJava.chap02;
+
+class Sum {
+  public static void main(String[] args) {
+    System.out.println("57 + 32 =" + (57 + 32));
+    System.out.println("57 + 32 =" + 89);
+    System.out.println("57 + 32 =" + "89");
+    System.out.println("57 + 32 = 89");
+  }
+}
