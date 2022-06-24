539c638bb7cfef84ab7dbb18e63a79fdc5394641
aki10-10 <97033820+aki10-10@users.noreply.github.com>
2022-06-24 15:35:02 +0900

    Sum2.java

---
+++ MeikaiJava/chap02/Sum2.java
@@ -0,0 +1,9 @@
+package MeikaiJava.chap02;
+
+class Sum2 {
+  public static void main(String[] args) {
+    System.out.println("57 + 32 =" + (57 + 32));
+  }
+}
+/*57 整数リテラル　57という1個の整数値
+"57" 文字列リテラル　5と7という2個の文字の並び*/
