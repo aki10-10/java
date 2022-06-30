9aaf8e7d23a48feaa79762f52964e25422bfcdc8
aki10-10 <97033820+aki10-10@users.noreply.github.com>
2022-06-30 12:00:00 +0900

    Variable.java

---
+++ MeikaiJava/chap02/Variable.java
@@ -0,0 +1,9 @@
+package MeikaiJava.chap02;
+
+class Variable {
+  public static void main(String[] args) {
+    int x; //xはint型の変数
+    x = 63; //xに63を代入
+    System.out.println(x); //xの値を表示
+  }
+}
