f61f29fb1d8833a96c3bdd13abad4fce18d22356
aki10-10 <97033820+aki10-10@users.noreply.github.com>
2022-07-01 13:06:55 +0900

    SumAve1.java

---
+++ MeikaiJava/chap02/SumAve1.java
@@ -0,0 +1,14 @@
+package MeikaiJava.chap02;
+
+class SumAve1 {
+  public static void main (String[] args) {
+    int x; //xはint型の変数
+    int y;//yはint型の変数
+    x =63;//xは63を代入
+    y=18;//yに18を代入
+    System.out.println("xの値は"+ x +"です");
+    System.out.println("yの値は" + y +"です");
+  }
+}
+//コンマ文字,で変数名を区切れば二つの宣言文を一つにまとめられる。
+// int x,y; int型の変数xとyを一度に宣言
