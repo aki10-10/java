746d7dc07d32d72ee4dd430071b6a650925192f3
aki10-10 <97033820+aki10-10@users.noreply.github.com>
2022-07-03 20:42:25 +0900

    SumAve2.java

---
+++ MeikaiJava/chap02/SumAve2.java
@@ -0,0 +1,13 @@
+package MeikaiJava.chap02;
+
+class SumAve2 {
+  public static void main(String[] args) {
+    int x;  //xはint型の整数
+    int y;  //yはint型の整数
+
+    System.out.println("xの値は" + x + "です。"); //xの値を表示
+    System.out.println("yの値は" + y +"です。");  //yの値を表示
+    System.out.println("合計は" + (x+y) + "です。");  //合計を表示
+    System.out.println("平均は" + (x+y) / 2 + "です。");  //平均を表示
+  }
+}
