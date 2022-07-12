8b13464544f91c5343a3aed3e58341130d59230a
aki10-10 <97033820+aki10-10@users.noreply.github.com>
2022-07-12 23:56:15 +0900

    AirthDouble.java

---
+++ MeikaiJava/chap02/AirthDouble.java
@@ -0,0 +1,22 @@
+package MeikaiJava.chap02;
+
+import java.util.Scanner;
+
+class AirthDouble {
+  public static void main (String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    System.out.println("xとyを加減乗除します。");
+
+    System.out.print("xの値:");
+    double x =stdIn.nextDouble();
+
+    System.out.print("yの値:");
+    double y =stdIn.nextDouble();
+
+    System.out.println("x+y="+(x+y));
+    System.out.println("x-y="+(x-y));
+    System.out.println("x*y="+(x*y));
+    System.out.println("x/y="+(x/y));
+    System.out.println("x%y="+(x%y));
+  }
+}
--- MeikaiJava/chap02/Minus1.java
+++ MeikaiJava/chap02/Minus1.java
@@ -5,7 +5,7 @@ class Munus1 {
   public static void main(String[] args) {
     Scanner stdIn = new Scanner(System.in);
 
-    System.out.print("整数値:");-
+    System.out.print("整数値:");
     int a = stdIn.nextInt();
 
     int b = -a;
